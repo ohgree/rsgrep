1760a699ea8bee16bec4980d99c61dd6d9c346eb
Minjun Shin <ohgree@u.sogang.ac.kr>
2020-08-04 02:09:04 +0900

    refactored config to new structure

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(filename).expect("Error while reading file");
+    let contents = fs::read_to_string(config.filename).expect("Error while reading file");
 
-    println!("contents: {:?}", contents);
+    println!("contents: {:}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
