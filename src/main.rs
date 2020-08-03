a8f00569e3956ede0fed6e2876baf9e95abc681b
Minjun Shin <ohgree@u.sogang.ac.kr>
2020-08-04 04:38:43 +0900

    write error message to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use rsgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = rsgrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
